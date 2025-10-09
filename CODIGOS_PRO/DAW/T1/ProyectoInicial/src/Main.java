31ba9e53f17491969863579a587b5e24b0af76ef
Develop and System <developandsystem@gmail.com>
2025-10-09 19:57:47 +0200

    update

--- CODIGOS_PRO/DAW/T1/ProyectoInicial/src/Main.java
+++ CODIGOS_PRO/DAW/T1/ProyectoInicial/src/Main.java
@@ -1,5 +1,4 @@
 public class Main {
-
     // esto es un comentario
     /*
     este comentario
@@ -7,16 +6,59 @@ public class Main {
     varias lineas de codigo
      */
     // TODO me falta esto
+    int edadGlobal = 12;
+
 
     public static void main(String[] args) {
-        System.out.println("Esta es la segunda linea del programa\n");
-        System.out.println("Este es mi primer programa en Java\n");
+        System.out.println("Primer programa en JAVA");
+
+        String miNombre = "Borja"; // null
+        Character letraCompleja = 'a';
+        char letraDNI = 'A';
+        Integer edadCompleja = 12;
+        int edad = 41;
+
+
+        final String DNI = "123123A";
+        Double alturaCompleja = 21.76;
+        double altura = 1.71;
+        Float pesoCompleja = 21.12f;
+        float peso = 70.7f;
+        Boolean experienciaCompleja = false;
+        boolean experiencia = true;
+
+        edad = 23;
+        letraDNI = 109;
+        miNombre = "Borja M. H.";
+        altura = 1.75;
+        peso = 67.5f;
+        experiencia = false;
+
+        System.out.println("Mi nombre es: " + miNombre);
+        System.out.println("La letra de mi dni es " + letraDNI);
+        System.out.println("Mi edad es " + edad + " años");
+        System.out.println("Mi altura es de " + altura + " cm");
+        System.out.println("Mi peso es de " + peso + " kg");
+        System.out.println("Mi experiencia es: " + experiencia);
+        System.out.println("Mi DNI es: " + DNI);
+        System.out.println("El valor del numero PI es " + Math.PI);
+        System.out.println("El valor del numero E es " + Math.E);
+        System.out.println("El valor maximo de un int es " + Integer.MAX_VALUE);
+        System.out.println("El valor minimo de un int es " + Integer.MIN_VALUE);
+
+
     }
 
     public void nombreMetodo() {
-
+        int edadMetodo = 12;
+        System.out.println(edadMetodo);
+        System.out.println(edadGlobal);
     }
 
+    public void nombreMetodo1(int edadGlobal) {
+        int edadMetodo = 123;
+        System.out.println(edadMetodo);
+        System.out.println(this.edadGlobal);
+    }
     // TODO esto tambien esta pendiente
-
 }
---
+++ CODIGOS_PRO/DAW/T1/ProyectoInicial/src/MainMetodos.java
@@ -0,0 +1,16 @@
+public class MainMetodos {
+
+    public static void main(String[] args) {
+        System.out.println("Main para probar metodos y variables");
+        // saludar("Dani");
+        saludar("Laura");
+        //despedir();
+    }
+    static void saludar(String data){
+        System.out.println("Enhorabuena "+data+" has llamado a tu primer metodo");
+    }
+
+     static void despedir(){
+        System.out.println("Hasta luego, hemos terminado");
+    }
+}
