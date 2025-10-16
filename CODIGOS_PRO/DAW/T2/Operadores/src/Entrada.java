4f843c1fd49401de2d4d0a994b3ce99e4ccd44b3
Develop and System <developandsystem@gmail.com>
2025-10-16 23:12:43 +0200

    update

---
+++ CODIGOS_PRO/DAW/T2/Operadores/src/Entrada.java
@@ -0,0 +1,10 @@
+public class Entrada {
+
+    public static void main(String[] args) {
+        Operadores operadoresVariable = new Operadores();
+        // operadoresVariable.operadoresAritmeticos();
+        // operadoresVariable.operadoresComparacion();
+        // operadoresVariable.operadoresLogicos();
+        operadoresVariable.evaluarCandidato();
+    }
+}
